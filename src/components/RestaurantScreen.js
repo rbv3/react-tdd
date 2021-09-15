bdc8d9b9667f4834eb0cfe976d80d0eb0e1c2030
rodrigovalenca <rodrigovalenca@thoughtworks.com>
2021-09-15 17:49:01 -0300

    styling components

--- src/components/RestaurantScreen.js
+++ src/components/RestaurantScreen.js
@@ -1,11 +1,16 @@
 import RestaurantList from './RestaurantList';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 const RestaurantScreen = () => {
   return (
-    <>
-      <h1>Restaurants</h1>
-      <RestaurantList />
-    </>
+    <Card>
+      <CardContent>
+        <Typography variant="h5">Restaurants</Typography>
+        <RestaurantList />
+      </CardContent>
+    </Card>
   );
 };
 export default RestaurantScreen;
