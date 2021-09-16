c315dfeab70a0a2c6db6205d242c0c5bc010f00a
rodrigovalenca <rodrigovalenca@thoughtworks.com>
2021-09-16 11:46:49 -0300

    loading behaviour added

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -4,23 +4,28 @@ import {loadRestaurants} from '../store/restaurants/actions';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-export const RestaurantList = ({loadRestaurants, restaurants}) => {
+export const RestaurantList = ({loadRestaurants, restaurants, loading}) => {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
   return (
-    <List>
-      {restaurants.map(restaurant => (
-        <ListItem key={restaurant.id}>
-          <ListItemText>{restaurant.name}</ListItemText>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {loading && <CircularProgress data-testid="loading-indicator" />}
+      <List>
+        {restaurants.map(restaurant => (
+          <ListItem key={restaurant.id}>
+            <ListItemText>{restaurant.name}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 const mapStateToProps = state => ({
   restaurants: state.restaurants.records,
+  loading: state.restaurants.loading,
 });
 
 const mapDispatchToProps = {loadRestaurants};
--- src/store/__tests__/restaurants.spec.js
+++ src/store/__tests__/restaurants.spec.js
@@ -4,28 +4,68 @@ import {loadRestaurants} from '../restaurants/actions';
 import restaurantsReducer from '../restaurants/reducers';
 
 describe('restaurants', () => {
-  describe('loadRestaurants action', () => {
-    it('stores the restaurants', async () => {
-      const records = [
-        {id: 1, name: 'Sushi Place'},
-        {id: 2, name: 'Pizza Place'},
-      ];
-
-      const api = {
-        loadRestaurants: () => Promise.resolve(records),
-      };
-      const initialState = {
-        records: [],
-      };
+  describe('initially', () => {
+    it('does not have the loading flag set', () => {
+      const initialState = {};
 
       const store = createStore(
         restaurantsReducer,
         initialState,
-        applyMiddleware(thunk.withExtraArgument(api)),
+        applyMiddleware(thunk),
       );
 
-      await store.dispatch(loadRestaurants());
-      expect(store.getState().records).toEqual(records);
+      expect(store.getState().loading).toEqual(false);
+    });
+  });
+  describe('loadRestaurants action', () => {
+    describe('while loading', () => {
+      it('sets a loading flag', () => {
+        const api = {
+          loadRestaurants: () => new Promise(() => {}),
+        };
+
+        const initialState = {};
+
+        const store = createStore(
+          restaurantsReducer,
+          initialState,
+          applyMiddleware(thunk.withExtraArgument(api)),
+        );
+
+        store.dispatch(loadRestaurants());
+
+        expect(store.getState().loading).toEqual(true);
+      });
+    });
+    describe('when loading succeeds', () => {
+      const records = [
+        {id: 1, name: 'Sushi Place'},
+        {id: 2, name: 'Pizza Place'},
+      ];
+      let store;
+      beforeEach(() => {
+        const api = {
+          loadRestaurants: () => Promise.resolve(records),
+        };
+
+        const initialState = {
+          records: [],
+        };
+
+        store = createStore(
+          restaurantsReducer,
+          initialState,
+          applyMiddleware(thunk.withExtraArgument(api)),
+        );
+
+        return store.dispatch(loadRestaurants());
+      });
+      it('stores the restaurants', () => {
+        expect(store.getState().records).toEqual(records);
+      });
+      it('clears the loading flag', () => {
+        expect(store.getState().loading).toEqual(false);
+      });
     });
   });
 });
