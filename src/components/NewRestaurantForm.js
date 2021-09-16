32b6977839ddd9114987d3cecb2b0e7b1c45d1cc
rodrigovalenca <rodrigovalenca@thoughtworks.com>
2021-09-16 18:26:35 -0300

    setting up store to add new restaurants

---
+++ src/components/NewRestaurantForm.js
@@ -0,0 +1,39 @@
+import {Button} from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import {useState} from 'react';
+import {connect} from 'react-redux';
+import {createRestaurant} from '../store/restaurants/actions';
+
+export const NewRestaurantForm = ({createRestaurant}) => {
+  const [name, setName] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    createRestaurant(name);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        value={name}
+        onChange={e => setName(e.target.value)}
+        placeholder="Add Restaurant"
+        fullWidth
+        variant="filled"
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        data-testid="new-restaurant-submit-button"
+      >
+        Add
+      </Button>
+    </form>
+  );
+};
+
+const mapStateToProps = null;
+const mapDispatchToProps = {createRestaurant};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewRestaurantForm);
